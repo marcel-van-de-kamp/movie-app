a7820e9612204c4ed07d7f5000c48ceeaff5bebf
Marcel van de Kamp <mailme@marcel-k.net>
2016-11-30 14:41:59 +0100

    movie model

--- src/app/movies/movie.ts
+++ src/app/movies/movie.ts
@@ -1,6 +1,47 @@
+ 
+
+/**
+ * NOTE: properties are optional with below syntax when explicitly creating a new instance
+ * and can only be assigned after creating a new instance:
+ *
+ * let movie = new Movie();
+ * movie.id = 1;
+ * movie.name = ...;
+ * 
+ * @export
+ * @class Movie
+ */
+// export class Movie {
+//   id: number;
+//   name: string;
+//   genre: string;
+//   rating: number;
+// }
+
+/**
+ * NOTE: properties can be made required when explicitly creating a new instance with a constructor:
+ * 
+ * let movie = new Movie(1, 'hello', 'horror', 6)
+ * 
+ * @export
+ * @class Movie
+ * @param id
+ * @param name
+ * @param genre
+ * @param rating
+ */
 export class Movie {
-  id: number;
-  name: string;
-  genre: string;
-  rating: number;
+  public id: number;
+  public name: string;
+  public genre: string;
+  public rating: number;
+
+    constructor(id:number, name: string, genre: string, rating: number) {
+        //assigning CONSTRUCTOR FUNCTION PARAMETER values to CLASS PROPERTIES
+        this.id = id;
+        this.genre = genre;
+        this.name = name;
+        this.rating = rating;
+    }
 }
+
