fc1d9fd6fe59e73b5a8fbdbcc16b6ae3d0ed53ad
Marcel van de Kamp <mailme@marcel-k.net>
2016-12-12 13:58:01 +0100

    move loading bar for later use (observables)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoadingBarComponent } from './loading-bar/loading-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoadingBarComponent],
-  exports: [LoadingBarComponent]
 })
 export class SharedModule { }
