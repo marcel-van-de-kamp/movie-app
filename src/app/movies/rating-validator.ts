f791936ea3d4f0dcc6637b48aba964c940a2ea4a
Mark Poelstra <mark.poelstra@clockwork.nl>
2018-01-17 12:46:35 +0100

    updated angular cli config and angular version.

--- src/app/movies/rating-validator.ts
+++ src/app/movies/rating-validator.ts
@@ -3,7 +3,6 @@ import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 export function ratingValidator(minVal: number, maxVal:Number): ValidatorFn {
   return function (control: AbstractControl): ValidationErrors {
     let value = control.value;
-    debugger;
     if (value < minVal || value > maxVal) {
       return {
         validateRating: true
